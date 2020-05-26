ab2609185bd01c22b895f82d4182f5f51a5fdaa7
Sean Bordelon <sean.j.bordelon@gmail.com>
2020-05-26 15:14:26 +0000

    Automatically backed up by Learn

--- lib/example.rb
+++ lib/example.rb
@@ -1,16 +1,6 @@
 # Use this space to code along with the readme
-def say_hello_world_ten_times
-  phrase = "Hello World!"
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
-  puts phrase
+def greeting
+  puts "Hello World"
 end
 
-say_hello_world_ten_times
+greeting
